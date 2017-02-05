ecc3e5c6a1d40bdb3e0c44adac90fd882cba1860
smckeon <mckeon.sean@outlook.com>
2017-02-05 15:34:57 -0500

    thus far

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,14 @@
+// Addons
 var $ = require ('jquery');
 var _ = require ('underscore');
 var githubtoken = require ('./gitapikey.js');
 var Handlebars = require ('handlebars');
+// var moment = require('moment');
+
+//
+var infoApi = 'https://api.github.com/users/smckeon';
+var repoApi = 'https://api.github.com/users/smckeon/repos';
+
 
 // send auth token to github if token is provided... which it is.
 if (githubtoken !== undefined) {
@@ -14,10 +21,9 @@ if (githubtoken !== undefined) {
 }
 
 // Aside
-var asideSource = $('#github-aside-template').html();
-var asideTemplate = Handlebars.compile(asideSource);
-
-$.ajax('https://api.github.com/users/smckeon').done(function(data){
+$.ajax(infoApi).done(function(data){
+  var asideSource = $('#github-aside-template').html();
+  var asideTemplate = Handlebars.compile(asideSource);
   var asideItem = {
     avatar: data.avatar_url,
     name: data.name,
@@ -30,11 +36,15 @@ $.ajax('https://api.github.com/users/smckeon').done(function(data){
 });
 
 // Repos
-
-$.ajax('https://api.github.com/users/smckeon/repos').done(function(data){
-
+$.ajax(repoApi).done(function(data){
+console.log(data);
   var source = $('#github-repos-template').html();
   var template = Handlebars.compile(source);
 
-  $('.github-repos').append(template(data));
+  data.forEach(function(item){
+  console.log(item);
+
+    $(".github-repos").append(template(item));
+
+  })
 });
